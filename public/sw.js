9fff9fe6f587660c3a295e78b6d0ea8fe7a3ea0e
j-cole-guru <alieamadusesay555@gmail.com>
2026-05-31 00:38:09 -1200

    Fix PWA manifest and service worker

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,39 @@
-const CACHE_NAME = 'mbhs-edunexus-v1'
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.png',
-  '/manifest.json'
-]
+const CACHE_NAME = 'mbhs-edunexus-v2'
 
 self.addEventListener('install', event => {
+  self.skipWaiting()
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache)
-    })
-  )
-})
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response
-      return fetch(event.request)
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/favicon.png'
+      ])
     })
   )
 })
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
       )
-    })
+    ])
+  )
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
+        return response
+      })
+      .catch(() => caches.match(event.request))
   )
 })
